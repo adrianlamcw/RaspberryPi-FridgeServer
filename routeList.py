f7728e3629050e492a5f6e4c9955d62f2b197c95
Adrian Lam <adrianlamcw@gmail.com>
2023-02-16 13:26:25 -0500

    add routes

--- routeList.py
+++ routeList.py
@@ -11,10 +11,22 @@ def hello():
       }
    return render_template('index.html', **templateData)
    
-@app.route("/test_endpoint", methods=["GET"])
+@app.route("/ping", methods=["GET"])
 def test_endpoint():
    data = {"key1": "value1", "key2": "value2"}
    return jsonify(data)
+
+@app.route("/check_groceries/<username>", methods=["GET"])
+def check_groceries(username):
+   return f"Hi, {username}"
+
+@app.route("/lock/<username>", methods=["GET"])
+def lock(username):
+   return f"Lock, {username}"
+
+@app.route("/unlock/<username>", methods=["GET"])
+def lock(username):
+   return f"Unlock, {username}"
 
 if __name__ == "__main__":
    app.run(host='10.40.82.177', port=80, debug=True)
